5b4a56eea9fd6569bb9c15bbc2244551666cafc3
egor <egorkubrakov87@gmail.ru>
2025-01-16 20:20:20 +0300

    fux manifest url

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -25,7 +25,7 @@ export function Root() {
   return (
     <ErrorBoundary fallback={ErrorBoundaryError}>
       <TonConnectUIProvider
-        manifestUrl={'public/tonconnect-manifest.json'}
+        manifestUrl={'/public/tonconnect-manifest.json'}
       >
         <App/>
       </TonConnectUIProvider>
